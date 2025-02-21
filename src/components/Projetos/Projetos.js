56bc3dc880a65f7369caac8a80e4b2546836eef0
Paulo Marsiglia <pmarsiglia93@gmail.com>
2025-02-21 01:23:22 -0300

    ajuste nos projetos e adicao de icones

--- src/components/Projetos/Projetos.js
+++ src/components/Projetos/Projetos.js
@@ -10,6 +10,8 @@ const projetos = [
   {
     id: 1,
     titulo: 'Bikcraft',
+    descricao: 'Bikcraft é um projeto de e-commerce para bicicletas elétricas de alta performance.',
+    tecnologias: ['HTML', 'CSS', 'JavaScript'],
     imagem: projeto1,
     site: 'https://bikcraft-psi-beige.vercel.app/',
     repositorio: 'https://github.com/pmarsiglia93/bikcraft'
@@ -17,6 +19,8 @@ const projetos = [
   {
     id: 2,
     titulo: 'Pesquisa Ativa',
+    descricao: 'Plataforma para análise de mercado e insights estratégicos.',
+    tecnologias: ['HTML', 'CSS', 'JS', 'React'],
     imagem: projeto2,
     site: 'https://pesquisa-ativa-negocios-sigma.vercel.app/',
     repositorio: 'https://github.com/pmarsiglia93/pesquisa-ativa-negocios'
@@ -24,6 +28,8 @@ const projetos = [
   {
     id: 3,
     titulo: 'Filmes API',
+    descricao: 'Aplicação para busca e favoritos de filmes consumindo API externa.',
+    tecnologias: ['HTML', 'CSS','Javascript', 'API Rest'],
     imagem: projeto3,
     site: 'https://case-stalse.vercel.app/',
     repositorio: 'https://github.com/pmarsiglia93/case-stalse'
@@ -31,6 +37,8 @@ const projetos = [
   {
     id: 4,
     titulo: 'Wishlist',
+    descricao: 'Plataforma para criar e gerenciar listas de desejos para e-commerce.',
+    tecnologias: ['HTML', 'CSS', 'Javascript'],
     imagem: projeto4,
     site: 'https://case-b8one.vercel.app/',
     repositorio: 'https://github.com/pmarsiglia93/case-b8one'
@@ -59,6 +67,12 @@ const Projetos = () => {
             <span className="close" onClick={() => setModalProjeto(null)}>&times;</span>
             <img src={modalProjeto.imagem} alt={modalProjeto.titulo} className="modal-img" />
             <h3>{modalProjeto.titulo}</h3>
+            <p className="modal-descricao">{modalProjeto.descricao}</p>
+            <div className="modal-tecnologias">
+              {modalProjeto.tecnologias.map((tech, index) => (
+                <span key={index} className="tech-tag">{tech}</span>
+              ))}
+            </div>
             <div className="modal-buttons">
               <a href={modalProjeto.site} target="_blank" rel="noopener noreferrer" className="btn">Ver Site</a>
               <a href={modalProjeto.repositorio} target="_blank" rel="noopener noreferrer" className="btn">Ver Repositório</a>
